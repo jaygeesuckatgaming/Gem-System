d5d86b937a939148b75689197721e219bb555f6a
jaygeesuckatgaming <jaygeesuckatgaming@gmail.com>
2026-08-11 06:46:30 +0200

    Remove hardcoded --voice alba argument to use config file

--- start_scripts/Start_PocketTTS.bat
+++ start_scripts/Start_PocketTTS.bat
@@ -3,5 +3,5 @@ TITLE Pocket TTS Server
 cd C:\Users\jayge\Documents\AI\Gem-System\pocket-tts
 call C:\Users\jayge\miniconda3\Scripts\activate.bat
 call conda activate pocket-tts
-call python server.py --port 13301 --voice alba
+call python server.py --port 13301
 cmd /k
